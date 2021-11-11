c756be9ec1a9ee8e5dd34a1e365c7068a6e717fc
hackdante <genoma3d@gmail.com>
2021-11-11 16:45:14 -0500

    React Router is working => Begin with best Routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch, Route
+} from 'react-router-dom'
+import { LogingPage } from '../components/auth/LogingPage'
+import { RegisterPage } from '../components/auth/RegisterPage'
+import { HomePage } from '../components/home/HomePage'
+import { JournalPage } from '../components/journal/JournalPage'
+
 
 
 export const AppRouter = () => {
     return (
         <div>
             <h1>App Router</h1>
+
+
+            <Router>
+                <div>
+                    <Switch>
+                        <Route exact path='/' component={HomePage} />
+                        <Route exact path='/login' component={LogingPage} />
+                        <Route exact path='/register' component={RegisterPage} />
+                        <Route exact path='/journal' component={JournalPage} />
+
+                    </Switch>
+
+                </div>
+
+            </Router>
         </div>
 
     )
